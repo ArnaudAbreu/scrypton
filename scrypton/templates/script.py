a2a976f6b48c4826c903c7eb47ee8644690cd5d4
Arnaud Abreu <arnaud.abreu.p@gmail.com>
2025-01-08 09:58:45 +0100

    add: color in logs

--- scrypton/templates/script.py
+++ scrypton/templates/script.py
@@ -5,20 +5,44 @@ import argparse
 import logging
 from logging.handlers import RotatingFileHandler
 
+
+reset_color = "\x1b[0m"
+dracula_cyan = "\x1b[38;5;159m"
+dracula_green = "\x1b[38;5;120m"
+dracula_orange = "\x1b[38;5;222m"
+dracula_red = "\x1b[38;5;210m"
+c_format_str = '%(name)s - [%(levelname)s] - %(message)s'
+
+FORMATS = {
+    logging.DEBUG: dracula_cyan + c_format_str + reset_color,
+    logging.INFO: dracula_green + c_format_str + reset_color,
+    logging.WARNING: dracula_orange + c_format_str + reset_color,
+    logging.ERROR: dracula_red + c_format_str + reset_color,
+    logging.CRITICAL: dracula_red + c_format_str + reset_color
+}
+
+
+class CustomFormatter(logging.Formatter):
+
+    def format(self, record):
+        log_fmt = FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
+file_formatter = logging.Formatter(
     "%(asctime)s :: %(levelname)s :: %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
 )
 file_handler = RotatingFileHandler(
     "{{slug}}.log", "a", 1000000, 1
 )  # 1Mo max, 1 backup
 file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
+file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
-stream_handler.setFormatter(formatter)
+stream_handler.setFormatter(CustomFormatter())
 logger.addHandler(stream_handler)
 
 
